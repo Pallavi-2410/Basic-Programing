e90e8a98db999b60360f33de6e6f3a23c74e4bc4
Pallavi-2410 <patel.pallavi1819@gmail.com>
2024-12-04 19:52:43 +0530

    primeNumber

--- index.js
+++ index.js
@@ -7,13 +7,3 @@ function parameter(num) {
 }
 console.log(parameter(12));
 
-function evenOdd(num){
-    for(let i=0; i<=num; i++){
-        if(i%2 == 0){
-            return true
-        }else{
-            return false
-        }
-    }
-}
-console.log(evenOdd(12))
